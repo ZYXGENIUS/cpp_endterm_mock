625cbe73cabf54d32fb872bf30bb98aeb88279e0
Lee_Jane <genius957hunter@gmail.com>
2025-01-03 22:04:38 +0800

    master

--- 7-2.cpp
+++ 7-2.cpp
@@ -1,12 +1,30 @@
 #include<iostream>
 #include<vector>
 #include<string>
+
 using namespace std;
 
 int main()
 {
     string a,b;
+    int d;
+    cin>>d;
     cin>>a>>b;
-    
+    bool check = true;
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]!=b[b.size()-1-i])
+        {
+            check = false;
+        }
+    }
+    if(check)
+    {
+        cout<<a<<" and "<<b<<" are reverse.";
+    }
+    else
+    {
+        cout<<a<<" and "<<b<<" are not reverse.";
+    }
     return 0;
 }
